35943d92842d3755dc8658233a8ae4a602ee4f7f
Involure <martinjanin20@gmail.com>
2017-02-23 23:25:07 +0100

    I completly changed my mind. I abandonned poo because it was useless and only costed time. The filterArr function is finally able to filter both boolean and integers arrays. Operators on boolean arrays fixed. Found a way to produce a linking pixels array with only array operations, the same way filters work. It isn't working yet thought. Isp ploting is down. I have not yet modified it to fit the global changes I made. Soon done, the things remaining to do are: - fixing the diminish function - implementing discrete fourier transform and functions using it

--- python/analyse_image/imclass.py
+++ python/analyse_image/imclass.py
@@ -13,7 +13,7 @@ bint = np.int32 #numpy signed integer type corresponding
 
 #ploting functions
 
-def _plotisp(subp, spectrum, c, n):
+def _plotIsp(subp, spectrum, c, n):
     """Plot the intensity spectrum to the specified subplot
     c is the color of the plot
     n is its name
@@ -26,6 +26,15 @@ def _plotisp(subp, spectrum, c, n):
     subp.set_title(n)
     subp.set_xlabel("pixel value")
     subp.set_ylabel("nb of pixels")
+
+def _plotArr(arr):
+    """Display the image."""
+    fig = plt.figure()
+    subp = fig.add_subplot(111)
+    subp.imshow(np.float64(arr) / 255)
+    subp.axis('off')
+    fig.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1)
+    fig.show()
 
 #array of integers converting functions
 
@@ -79,21 +88,22 @@ def expend(array):
 #the actual class
 
 class Image:
-    """A custom Image class, mainly an RGB array."""
+    """A custom Image class. Either a RGB or grey levels image"""
 
-    def __init__(self, name, array, shape = None, _isp = emptya, _fsp = emptya):
+    def __init__(self, name, array, isColour):
         """Takes an unsigned little integer array as an argument."""
         isul(array)
+
+        self.shape = np.shape(array)
+        dim = len(self.shape)
+        self.isColour = False
+        if dim == 3:
+            self.isColour = True
+        elif dim != 2:
+            raise AttributeError("dimension of the array isn't 2 or 3")
+
         self.array = expend(array)
-        if shape == None:
-            self.shape = np.shape(array)
-        else:
-            self.shape = shape
         self.name = name
-        if (_isp != emptya).any():
-            self._isp = _isp
-        if (_fsp != emptya).any():
-            self._fsp = _fsp
 
     def __str__(self):
         return ("%s ; size %s"%(self.name, self.shape))
@@ -101,88 +111,72 @@ class Image:
     def __repr__(self):
         return ("%s ; size %s"%(self.name,self.shape))
 
-    def _initIsp(self):
-        """Initialize the itensity spectrum."""
-        self._isp = np.zeros((N,3), dtype = ubint)
-#        for i in range(self.shape[0]):
-#            for j in range(self.shape[1]):
-#                for colour in range(3):
-#                    self._isp[self.array[i,j,colour], colour] += 1
-        for i in range(N):
-            self._isp[i] = np.sum(self.array == i, axis = (0, 1))
-
     def isp(self):
-        """Return the intensity spectrum of the image"""
-        try:
-            return self._isp
-        except AttributeError:
-            self._initIsp()
-            return self._isp
-
-    def _ispExist(self):
-        """Return True only if the intensity spectrum is already initialized."""
-        try:
-            ignore = self._isp
-            return True
-        except AttributeError:
-            return False
-
-    def _fspExist(self):
-        """Return True only if the frenquency spectrum is already initialized."""
-        try:
-            ignore = self._fsp
-            return True
-        except AttributeError:
-            return False
-
-    def extract(self, colour):
-        """Return an image with only one chromatic componant."""
-        arr = np.zeros(self.shape, dtype = ulint)
-        arr[:,:,colour] = np.copy(self.array[:,:,colour])
-        isp = emptya
-        fsp = emptya
-        if self._ispExist():
+        """Return the intensity spectrum"""
+        if self.isColour:
             isp = np.zeros((N,3), dtype = ubint)
-            isp[:,colour] = np.copy(self._isp[:,colour])
-        if self._fspExist():
-            fsp = np.zeros(self.shape, dtype = ulint)
-            fsp[:,:,colour] = np.copy(self._fsp[:,:,colour])
-        return Image(self.name + "%d"%colour, arr, self.shape, isp, fsp)
+        else:
+            isp = np.zeros((N), dtype = ubint)
+        for i in range(N):
+            isp[i] = np.sum(self.array == i, axis = (0, 1))
+        return isp
 
     def show(self):
-        """Display the image."""
-        fig = plt.figure()
-        subp = fig.add_subplot(111)
-        subp.imshow(self.array)
-        subp.axis('off')
-        fig.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1)
-        fig.show()
+        _plotArr(self.array)
 
     def showc(self, colour):
         """Display only one chromatic color of the image."""
-        self.extract(colour).show()
+        if not self.isColour:
+            raise TypeError("not a coloured image")
+        _plotArr(self.array[:,:,colour])
 
     def showisp(self):
         """Display the intensity spectrum of the image."""
         isp = self.isp()
         fig = plt.figure()
-        sub1 = fig.add_subplot(131)
-        sub2 = fig.add_subplot(132)
-        sub3 = fig.add_subplot(133)
-        _plotisp(sub1, isp[:,0], "red", self.name)
-        _plotisp(sub2, isp[:,1], "green", self.name)
-        _plotisp(sub3, isp[:,2], "blue", self.name)
+        if self.isColour:
+            sub1 = fig.add_subplot(131)
+            sub2 = fig.add_subplot(132)
+            sub3 = fig.add_subplot(133)
+            _plotIsp(sub1, isp[:,0], "red", self.name)
+            _plotIsp(sub2, isp[:,1], "green", self.name)
+            _plotIsp(sub3, isp[:,2], "blue", self.name)
+        else:
+            sub = fig.add_subplot(111)
+            _plotIsp(sub, isp, "black", self.name)
         fig.show()
 
-    def save(self, fileName):
-        img.fromarray(self.array).convert('RGB').save(fileName)
+    #def save(self, fileName = ""):
+        #if fileName == "":
+            #fileName = self.name
+        #img.fromarray(self.array).convert('RGB').save(fileName)
 
-    def copy(self, name):
-        return Image(name, np.copy(self.array), self.shape, np.copy(self._isp), np.copy(self._fsp))
+class BinImage:
+
+    def __init__(self, name, array):
+        self.name = name
+        
+        shape = np.shape(array)
+        if shape != 2:
+            raise AttributeError("array dimension must be two")
+        self.shape = shape
+
+        if array.dtype != np.bool:
+            raise AttributeError("array must be boolean")
+        self.array = array
+
+    def __str__(self):
+        return ("%s ; size %s"%(self.name, self.shape))
+
+    def __repr__(self):
+        return ("%s ; size %s"%(self.name,self.shape))
+
+    def show(self):
+        _plotArr(np.float(self.array))
 
 #opening fonction
 
 def open(fileName):
     im = img.open(fileName)
     arr = np.array(im, dtype = ulint)
-    return Image(fileName, arr)
+    return Image(fileName, arr, True)
--- python/analyse_image/preprocess.py
+++ python/analyse_image/preprocess.py
@@ -1,92 +1,185 @@
-from filterBase import *
+import png
+import numpy as np
+import matplotlib.pyplot as plt
+Bcalc = 32 #number of bits of the hardware
+Bim = 8 #number of bits of the colors
+R = 2 ** Bcalc #range of the integers used for calculus
+N = 2 ** Bim #number of intensity levels in picture
+ulint = np.uint8 #numpy unsigned integer type corresponding
+ubint = np.uint32 #numpy unsigned integer type corresponding
+lint = np.int8 #numpy signed integer type corresponding
+bint = np.int32 #numpy signed integer type corresponding
 
-def equalize(im):
-    arr = np.copy(im.array)
-    ispRef = [[],[],[]] #list containing indexes of pixels sorted by intensity value
-    if im._ispExist() :
-        isp = np.copy(im.isp())
-    else:
-        isp = np.zeros((N,3), dtype = ubint)
-    #initializing ispRef lists
-    for i in range(N):
-        for colour in range(3):
-            ispRef[colour].append([])
-    #initializing ispRef
-    for i in range(im.shape[0]):
-        for j in range(im.shape[1]):
-            for colour in range(3):
-                intensity = arr[i,j,colour]
-                ispRef[colour][intensity].append((i,j))
-                if not im._ispExist():
-                    isp[intensity, colour] += 1
+#opening fonction
 
-    nbPix = im.shape[0] * im.shape[1]
-    q = nbPix // N
-    r = nbPix % N
-    equalRep = np.full((N), q, dtype = ubint) #desired intensity spectrum
-    #intializing equalRep
-    for i in range(r):
-        equalRep += 1
-
-    newIsp = np.zeros((N,3), dtype = ubint) #the isp of the futur equalized image
+def open(fileName):
+    tuple = png.Reader(fileName).asRGB8()
+    flatSize = (tuple[1], tuple[0] * 3)
+    iter = tuple[2]
+    flatArray = np.zeros(flatSize, dtype = ulint)
+    i = 0
+    for arr in iter:
+        flatArray[i] = np.fromiter(arr, dtype = ulint)
+        i += 1
+    size = (tuple[1], tuple[0], 3)
+    array = np.zeros(size, dtype = ulint)
     for colour in range(3):
-        remaining = equalRep[0] 
-        newI = 0
-        for intensity in range(1):
-            currentIPixNb = isp[intensity, colour]
-            #loop until desired newI is found
-            while  currentIPixNb > remaining:
-                currentIPixNb -= remaining
-                newI += 1
-                remaining = equalRep[newI]
-            remaining -= currentIPixNb
-            #updating newIsp and changing pixels value to match the desired repartition
-            newIsp[newI, colour] += isp[intensity, colour]
-            for pix in ispRef[colour][intensity]:
-                arr[pix[0], pix[1], colour] = newI
+        array[:,:,colour] = flatArray[:,colour::3]
+    return array
 
-    #setting the argument image isp in case it wasn't initialized
-    if not im._ispExist():
-        im._isp = isp
-        
-    return Image(im.name + '_equalized', arr, im.shape, newIsp)
+#array conversion and type checking functions
+
+def isul(array):
+    return array.dtype == ulint
+def isub(array):
+    return array.dtype == ubint
+def issl(array):
+    return array.dtype == lint
+def issb(array):
+    return array.dtype == bint
+def isbin(array):
+    return array.dtype == np.bool
+
+def cisul(array):
+    if not isul(array):
+        raise TypeError("not an unsigned little integer array")
+def cisub(array):
+    if not isub(array):
+        raise TypeError("not an unsigned big integer array")
+def cissl(array):
+    if not issl(array):
+        raise TypeError("not a signed little integer array")
+def cissb(array):
+    if not issb(array):
+        raise TypeError("not a signed big integer array")
+def cisbin(array):
+    if not isbin(array):
+        raise TypeError("not a boolean array")
+
+def reduce(array):
+    """Convert an unsigned big integer array into an unsigned little integer using the maximum intensity range"""
+    cisub(array)
+    s = np.min(array)
+    e = np.max(array)
+    r = e - s
+    newArr = np.copy(array)
+    if r != N:
+        if np.float64(e) * N < R: 
+            newArr = ulint((newArr * (N - 1)) // r)
+        else:
+            newArr = ulint(newArr * ((N - 1) // r))
+    return newArr
+
+def expend(array):
+    cisul(array)
+    s = np.min(array)
+    e = np.max(array)
+    r = e - s
+    newArr = np.copy(array)
+    if r < N - 1:
+        newArr = ulint((ubint(newArr) * (N - 1)) // r)
+    return newArr
+
+#ploting functions
+
+def plotIsp(subp, spectrum, c, n):
+    """Plot the intensity spectrum to the specified subplot
+    c is the color of the plot
+    n is its name
+    """
+    ind = np.arange(N)
+    width = 1
+
+    subp.bar(ind, spectrum, width, color = c, edgecolor = c)
+
+    subp.set_title(n)
+    subp.set_xlabel("pixel value")
+    subp.set_ylabel("nb of pixels")
+
+def show(arr):
+    """Display the image."""
+    size = np.shape(arr)
+    if len(size) == 2:
+        array = np.zeros((size[0], size[1], 3), dtype = arr.dtype)
+        for colour in range(3):
+            array[:,:,colour] = arr
+    else:
+        array = np.copy(arr)
+    if isul(array):
+        array = array / 255
+    elif not isbin(array):
+        raise TypeError("must be an unsigned little integer or a boolean array to be plot")
+    fig = plt.figure()
+    subp = fig.add_subplot(111)
+    subp.imshow(array)
+    subp.axis('off')
+    fig.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1)
+    fig.show()
+
+#type converting functions
+
+def greyAv(arr):
+    return ulint(np.sum(ubint(arr), dtype = ubint, axis = 2) // 3)
+
+def greyMax(arr):
+    return  np.max(arr, axis = 2)
+
+def binary(arr, cutIntensity):
+    return arr > cutIntensity
 
 #base operations on images
 
-def operate(im1, im2, f, opName):
-    if im1.shape != im2.shape:
-        raise ValueError("can't add images with different sizes")
-    shape = im1.shape
-    arr1 = bint(np.copy(im1.array))
-    arr2 = bint(np.copy(im2.array))
-    return Image(opName + "_" + im1.name + "_" + im2.name, f(arr1, arr2), shape)
+def operate(arr1, arr2, func):
+    if not ((isul(arr1) and isul(arr2)) or (isub(arr1) and isub(arr2))) :
+        raise TypeError("not same type or not integer array")
+    if np.shape(arr1) != np.shape(arr2) :
+        raise ValueError("can't operate on images with different sizes")
+    return func(arr1, arr2)
 
-def add(im1, im2):
-    return operate(im1, im2, (lambda x,y : x + y), "+")
+def add(arr1, arr2):
+    return operate(ubint(arr1), ubint(arr2), (lambda x,y : x + y))
 
-def sub(im1, im2):
-    return operate(im1, im2, (lambda x,y : x - y), "-")
+def sub(arr1, arr2):
+    return operate(ubint(arr1), ubint(arr2), (lambda x,y : x - y))
 
-def max(im1, im2):
-    return operate(im1, im2, np.maximum, "max")
+def max(arr1, arr2):
+    return operate(ubint(arr1), ubint(arr2), np.maximum)
 
-def min(im1, im2):
-    return operate(im1, im2, np.minimum, "min")
+def min(arr1, arr2):
+    return operate(ubint(arr1), ubint(arr2), np.minimum)
 
-def diff(im1, im2):
-    return operate(im1, im2, (lambda x,y : np.maximum(im1, im2) - np.minimum(im1, im2)), "diff")
+def diff(arr1, arr2):
+    return operate(ubint(arr1), ubint(arr2), (lambda x,y : np.maximum(arr1, arr2) - np.minimum(arr1, arr2)))
 
-def neg(im):
-    shape = im.shape
-    arr = np.copy(im.array)
-    isul(arr)
-    return Image("neg_" + im.name, -(arr + 1), shape)
+def neg(arr):
+    cisul(arr)
+    array = np.copy(arr)
+    return -(arr + 1)
+
+def operateBin(bin1, bin2, func):
+    cisbin(bin1)
+    cisbin(bin2)
+    if np.shape(bin1) != np.shape(bin2):
+        raise ValueError("can't operate on images with different size")
+    return func(bin1, bin2)
+
+def algOr(bin1, bin2):
+    return operateBin(bin1, bin2, (lambda x,y : x + y))
+
+def algAnd(bin1, bin2):
+    return operateBin(bin1, bin2, (lambda x,y : np.uint8(x) * np.uint8(y) == 1))
+
+def algXor(bin1, bin2):
+    return operateBin(bin1, bin2, (lambda x,y : np.uint8(x) + np.uint8(y) == 1))
+
+def algNot(bin):
+    cisbin(bin)
+    return bin == False
 
 #algebra operations
 
 def makeFilter(set):
-    if set.dtype != np.bool:
-        raise AttributeError("shape must be a boolean array")
+    cisbin(set)
     shape = np.shape(set)
     dx, dy = shape[0], shape[1]
     bigShape =(dx * 2 - 1, dy * 2 - 1)
@@ -97,37 +190,104 @@ def makeFilter(set):
                 result[i : dx + i, j : dy + j] += set 
     return result
 
-def algOp(im, func, baseValue, set):
-    setFilter  = lint(makeFilter(set))
-    arr = ubint(im.array)
-    arr = filterArr(arr, setFilter, func, baseValue)
-    return Image("closed_" + im.name, reduce(arr), im.shape)
+def algClose(bin, set):
+    filter = makeFilter(set)
+    return filterArr(bin, filter, (lambda x,y : y), algOr, False, np.bool)
 
-def algClose(im, set):
-    return algOp(im, np.maximum, 0, set)
+def algOpen(bin, set):
+    filter = (makeFilter(set))
+    return filterArr(bin, filter, (lambda x,y : y), algAnd, True, np.bool)
 
-def algOpen(im, set):
-    return algOp(im, np.minimum, 255, set)
+def path(ind):
+    if ind <= 2:
+        return (0,ind)
+    elif ind <= 4:
+        return (ind - 2, 2)
+    elif ind <= 6:
+        return (2, 6 - ind)
+    elif ind <= 8:
+        return (8 - ind, 0)
+    else:
+        raise ValueError("path index out of range (0-8)")
 
-def isLink(boolArr):
+def isFrontier(bin):
+    frontierFilter = np.array([[False, True, False],[True, True, True], [False, True, False]])
+    return algXor(bin, filterArr(bin, frontierFilter, (lambda x,y : y), algAnd, True, np.bool))
+
+def isLink(bin):
+    shape = np.shape(bin)
+    Dx, Dy = shape
+    bigShape = (Dx + 2, Dy + 2)
+    changeCount = np.zeros(shape, dtype = np.uint8)
+    currentVal = np.full(bigShape, True, dtype = np.bool)
+    currentVal[2:, 2:] = np.copy(bin)
+    for i in range(1, 9):
+        ind = path(i)
+        changeCount += np.uint8(algXor(currentVal[2 - ind[0] : 2 - ind[0] + Dx, 2 - ind[1] : 2 - ind[1] + Dy], currentVal[1 : 1 + Dx, 1 : 1 + Dy]))
+        currentVal[2 - ind[0] : 2 - ind[0] + Dx, 2 - ind[1] : 2 - ind[1] + Dy] = np.copy(bin)
+    return (currentVal >= 3)[1 : 1 + Dx, 1 : 1 + Dy]
+
+def diminish(bin):
+    return algAnd(algOr(algNot(Frontier), isLink), bin)
     
-
-def squeletize(binIm):
-    arr = np.bool(binIm.array)
-
 
 #filtering functions
 
 def isFilter(filterArr):
-    issl(filterArr)
     filterSize = np.shape(filterArr)
     if (filterSize[0] % 2 != 1) or (filterSize[1] % 2 != 1):
         raise ValueError("can't filter with an even sized filter array")
 
+def filterArr(arr, filterArr, filterFunc, addFunc, baseValue, arrType, finalFunc = None) :
+    isFilter(filterArr)
+    shape = np.shape(arr)
+    filterSize = np.shape(filterArr)
+
+    dx = (filterSize[0] - 1) // 2
+    dy = (filterSize[1] - 1) // 2
+    Dx = shape[0] + 2 * dx
+    Dy = shape[1] + 2 * dy
+    if len(shape) == 3:
+        bigShape = (Dx, Dy, 3)
+    else:
+        bigShape = (Dx, Dy)
+
+    result = np.full(bigShape, baseValue, dtype = arrType)
+    for i in range(-dx, dx + 1):
+        for j in range(-dy, dy + 1):
+            if ulint(filterArr[dx - i, dy - j]) != 0:
+
+                shiftedArr = np.zeros(bigShape, dtype = arrType)
+                if len(shape) == 3:
+                    shiftedArr[dx + i : dx + i + shape[0], dy + j : dy + j + shape[1], :] = filterFunc(filterArr[dx - i, dy - j], arr)
+                else:
+                    shiftedArr[dx + i : dx + i + shape[0], dy + j : dy + j + shape[1]] = filterFunc(filterArr[dx - i, dy - j], arr)
+                result = addFunc(result, shiftedArr)
+
+    if finalFunc != None:
+        result = finalFunc(result)
+    if len(shape) == 3:
+        return result[dx : shape[0] + dx, dy : shape[1] + dy, :]
+    else:
+        return result[dx : shape[0] + dx, dy : shape[1] + dy]
+
+def filterSum(arr, filterList):
+    """filter the array wit heach filter and add the result.
+    The array must be a signed big integer array."""
+    newFilterList = reduceFilterList(filterList)
+    result = filterArr(arr, filterList[0], (lambda x,y : x * y), np.add, 0, np.abs)
+    for filter in filterList[1:]:
+        cissl(filter)
+        result += filterArr(arr, filter, (lambda x,y : x * y), np.add, 0, np.abs)
+    return Image("filtered_" + im.name, reduce(result), im.shape)
+
 def addFilter(firstFilter, secondFilter):
-    """Return a new filter equivalent to applying successively the first and the second filter WITHOUT TAKING THE ABSOLUTE VALUE"""
+    """Return a new filter equivalent to applying successively the first and the second filter.
+    Filters must be signed little integers arrays of odd size"""
     isFilter(firstFilter)
     isFilter(secondFilter)
+    cissl(firstFilter)
+    cissl(secondFilter)
     shape1 = np.shape(firstFilter)
     shape2 = np.shape(secondFilter)
     dx = (shape1[0] - 1) // 2
@@ -141,87 +301,63 @@ def addFilter(firstFilter, secondFilter):
             result += shiftedArr
     return result
 
+def reduceFilterList(filterList):
+    """add consecutive positive filters in order to optimize calculus time.
+    operate on a signed little integer filterList"""
+    newFilterList = []
+    i = 1
+    filter = filterList[0]
+    while i < len(filterList):
+        nextFilter = filterList[i]
+        if type(filter) != list:
+            cissl(filter)
+            if type(nextFilter) != list and np.min(nextFilter) >= 0 and np.min(filter) >=0:
+                nextFilter = addFilter(filter, nextFilter)
+            else:
+                newFilterList.append(filter)
+        else:
+            newFilterList.append(filter)
+        i += 1
+        filter = nextFilter
+    newFilterList.append(filter)
+    return newFilterList
+                
 
-def filterArr(sourceArr, filterArr, func, baseValue):
-    """Take a unsigned big integer array as argument. Return the absolute value of the filtered array."""
-    isFilter(filterArr)
-    isub(sourceArr)
-    arr = np.copy(sourceArr)
-    shape = np.shape(sourceArr)
-    filterSize = np.shape(filterArr)
-    dx = (filterSize[0] - 1) // 2
-    dy = (filterSize[1] - 1) // 2
-    bigShape =(shape[0] + 2 * dx, shape[1] + 2 * dy, 3)
-    result = np.full(bigShape, baseValue, dtype = bint)
-    for i in range(-dx, dx + 1):
-        for j in range(-dy, dy + 1):
-            if filterArr[dx - i, dy - j] != 0:
-                shiftedArr = np.zeros(bigShape, dtype = bint)
-                shiftedArr[dx + i : dx + i + shape[0], dy + j : dy + j + shape[1], :] = filterArr[dx - i, dy - j] * arr
-                result = func(result, shiftedArr)
-    if np.min(filterArr) < 0:
-        result = np.abs(result)
-    return ubint(result[dx : shape[0] + dx, dy : shape[1] + dy, :])
-
-def filter(im, filterList):
-    """Filter the array of the image successively with all the filter contained in filterList"""
-    arr = ubint(im.array)
-    for filter in filterList:
-        arr = filterArr(arr, filter, np.add, 0)
-    return Image("filtered_" + im.name, reduce(arr), im.shape)
-
-def filterSum(im, filterList):
-    arr = ubint(im.array)
-    result = filterArr(arr, filterList[0], np.add, 0)
-    for filter in filterList[1:]:
-        result += filterArr(arr, filter, np.add, 0)
-    return Image("filtered_" + im.name, reduce(result), im.shape)
-
-#transforming an image to binary image
-
-def greyAv(im):
-    array = im.array
-    arrayAv = np.sum(ubint(array), dtype = ubint, axis = 2) // 3
-    greyArr = np.zeros(im.shape, dtype = ubint)
-    for colour in range(3):
-        greyArr[:,:,colour] = np.copy(arrayAv)
-    return Image("greyAv_" + im.name, reduce(greyArr), im.shape)
-
-def greyMax(im):
-    array = im.array
-    arrayMax = np.max(array, axis = 2)
-    greyArr = np.zeros(im.shape, dtype = ulint)
-    for colour in range(3):
-        greyArr[:,:,colour] = np.copy(arrayMax)
-    return Image("greyMax_" + im.name, greyArr, im.shape)
-
-def binary(im, cutIntensity):
-    arr = np.zeros(im.shape, dtype = ulint)
-    for colour in range(3):
-        arr[:,:,colour] = ulint(im.array[:,:,0] > cutIntensity) * 255
-    return Image("bin_" + im.name, arr, np.shape)
+def filter(arr, filterList):
+    """Filter the array with all the filters contained in filterList.
+    Array must be an unsigned little integer array."""
+    cisul(arr)
+    array = bint(np.copy(arr))
+    for filter in reduceFilterList(filterList):
+        if type(filter) == list:
+            filterSum(array, filter)
+        else:
+            array = filterArr(array, filter, (lambda x,y : x * y), np.add, 0, bint, np.abs)
+    return reduce(ubint(np.abs(array)))
 
 #maximize intensity while keeping color
 
-def colorize(im):
-    array = np.copy(im.array)
+def colorize(arr):
+    cisul(arr)
+    array = np.copy(arr)
+    shape = np.shape(array)
     maxCompArray = np.max(array, axis = 2)
-    divideArray = np.zeros(im.shape, dtype = ulint)
+    divideArray = np.zeros(shape, dtype = ulint)
     for colour in range(3):
         divideArray[:,:,colour] = maxCompArray
-    divideArray = np.maximum(divideArray, np.ones(im.shape, dtype = ubint))
+    divideArray = np.maximum(divideArray, np.ones(shape, dtype = ubint))
     array = (ubint(array) * 255) // divideArray
-    return Image("colorized_" + im.name, reduce(array), im.shape)
+    return array
 
-
-
-
-
-
-
-
-
-
-
-
+#im1 = open("image1.png")
+#im2 = open("image2.png")
+#blur = np.ones((5,5), dtype = lint)
+#corner = np.array([[1,0,1],[0,0,0],[1,0,1]], dtype = lint)
+#grad = [np.array([[1,1,1], [0,0,0], [-1, -1, -1]], dtype = lint), np.array([[1,0,-1], [1,0,-1], [1,0,-1]], dtype = lint)]
+#im3 = filter(im1, grad)
+#im4 = greyAv(im3)
+#bin1 = binary(im4, 20)
+testbin = np.full((1000,1000), False, dtype = np.bool)
+testbin[500] = np.full((1000), True, dtype = np.bool)
+#set1 = np.array([[True,True],[True,True]])
 
