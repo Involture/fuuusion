e60826bbfefd88df1e50630a304cfe7161aca251
Involure <martinjanin20@gmail.com>
2017-05-04 10:52:45 +0200

    updated reader to use function in dbase files completed fourier filter creating functions in reader

--- python/analyse_image/reader.py
+++ python/analyse_image/reader.py
@@ -19,9 +19,26 @@ def toArr(lines):
         rows.append(np.fromiter(valList, dtype = lint))
     return np.row_stack(rows)
 
+def toArg(l):
+    split = l
+    for i in range(len(split)):
+        if ',' in split[i]:
+            splitsplit = split[i].split(',')
+            split[i] = tuple([int(x) for x in splitsplit])
+        else:
+            split[i] = int(split[i])
+    return tuple(split)
+
+def fromFunc(func):
+    split = func[1:].split(" ")
+    return funcNames[split[0]](*toArg(split[1:]))
+    
 def fromSingle(string):
     split = string.split("\n")
-    return (split[0], toArr(split[1:]))
+    if split[1][0] == '>':
+        return (split[0], fromFunc(split[1]))
+    else:
+        return (split[0], toArr(split[1:]))
 
 def fromGroup(dic, l):
     split = l.split("\n")
@@ -60,13 +77,29 @@ def squareCut(shape, percentage):
     arr[ind] = np.ones(littleShape, dtype = ulint)
     return arr
 
+def vdist(shape):
+    p, q = shape[:2]
+    arr1 = np.stack([np.arange(p // 2) for i in range(q)], axis = 1)
+    arr2 = np.stack([np.arange(p // 2 + (p % 2)) for i in range(q)], axis = 1)
+    return np.concatenate((arr1, np.flipud(arr2)), axis = 0)
+
+def permute(arr):
+    row = arr[0]
+    temp = row[-1]
+    arr[0][1:] = row[:-1]
+    arr[0][0] = temp
+
+def ddist(size):
+    row = np.array(np.concatenate((np.arange(size), np.arange(size - 2, 0, -1)), axis = 0), ndmin = 2)
+    res = np.array(row)
+    for j in range(size - 1):
+        permute(row)
+        res = np.concatenate((res, row), axis = 0)
+    return res[:,:size]
+
 def ellipse(shape):
     p, q = shape[:2]
-    arr1 = np.stack([np.arange(p//2) for i in range(q)], axis = 1)
-    arr1 = np.concatenate((arr1, np.flipud(arr1)), axis = 0)
-    arr2 = np.stack([np.arange(q//2) for i in range(p)], axis = 0)
-    arr2 = np.concatenate((arr2, np.fliplr(arr2)), axis = 1)
-    arr = (arr1 * q) ** 2 + (arr2 * p) ** 2
+    arr = (vdist((p,q)) * q) ** 2 + (vdist((q,p)).T * p) ** 2
     for i in range(2, len(shape)):
         arr = np.stack([arr for i in shape[i]], axis = -1)
     return arr
@@ -75,21 +108,23 @@ def circleCut(shape, p0):
     return 20000 * ellipse(shape) <= (shape[0] * shape[1] * p0) ** 2
 
 def BW(shape, n, p0):
-    return (1 / (1 + (ellipse(shape) * 100 ** 2 / (shape[0] * shape[1] * p0) ** 2)))
+    return (1 / (1 + (ellipse(shape) * 160000 / (shape[0] * shape[1] * p0) ** 2)))
 
-def gaussCircle(shape, p0):
-    return np.exp(-ellipse(shape) * 100 ** 2 / (shape[0] * shape[1] * p0) ** 2)
+def cgauss(shape, p0):
+    return np.exp(-ellipse(shape) * 160000 / (shape[0] * shape[1] * p0) ** 2)
 
-def gauss(shape, p0, angle):
-    quad = angle // 90
-    if quad == 0:
-        t = (1,1)
-    elif quad == 1:
-        t = (-1, 1)
-    elif quad == 2:
-        t = (-1, -1)
-    else:
-        t = (1, -1)
-    ang = angle % 180
-    u = 
+def vgauss(shape, p0):
+    return np.exp(-vdist(shape) ** 2 * 40000 / (shape[0] * p0)  ** 2)
 
+def hgauss(shape, p0):
+    p, q = shape[:2]
+    return hgauss((q, p), p0).T
+
+def d1gauss(shape, p0):
+    assert(shape[0] == shape[1])
+    return np.exp(-ddist(shape[0]) ** 2 * 40000 / (shape[0] * p0) ** 2)
+
+def d2gauss(shape, p0):
+    return np.flipud(d1gauss(shape, p0))
+
+funcNames = locals()
