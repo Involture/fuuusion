01c35dbe0ab1c9b09031b4a015fd8f150054147c
TimDarcet <timothee.darcet@gmail.com>
2017-05-28 18:40:46 +0200

    silhouette tests*

--- python/modelisation3D/test.py
+++ python/modelisation3D/test.py
@@ -2,8 +2,8 @@ import wep
 
 
 c1 = wep.simpleCube()
-c2 = wep.simpleCube()
-c2.translate(wep.vector(1 / 2, 1 / 2, 1 / 2))
-c3 = c1.intersection(c2)
-print(c3)
-c3.plot(True)
+
+sil = c1.silhouettePhoto((-1, 1/2, 1/2), 0.1, (0.5, 0.5, 0))
+sil.plot()
+
+sil.cone(5).plot()
