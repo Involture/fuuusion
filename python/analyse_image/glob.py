localisation fixed. Yet the way I implemented it causes redondant sum computation during parabolic approximation. Will be fixed in next commit

python/analyse_image/glob.py
@@ -14,7 +14,7 @@ ccpl = np.complex64
 start = t.time()
 
 V = True
-VV = False
+VV = True
 
 def p(s):
     """Print only if V (standing for a verbose parametre) is True."""

python/analyse_image/ops.py
@@ -1,8 +1,10 @@
-from glob import *
-from fourier import fft, ifft, sf
 from scipy.misc import imread
 import matplotlib.pyplot as plt
 import itertools
+
+from glob import *
+from fourier import fft, ifft, sf
+from filts import *
 
 #image size processing
 
@@ -196,62 +198,72 @@ def filt(arr, f):
     
 #parabolic approximation
 
+def det(arr):
+    """Compute det of arr on first two dimensions."""
+    transTuple = tuple(range(2, arr.ndim)) + (0, 1)
+    tarr = np.transpose(arr, transTuple)
+    return np.linalg.det(tarr)
+
 def cramer(mat, vect):
     mat1 = np.stack((vect, mat[:,1], mat[:,2]), axis = 1)
     mat2 = np.stack((mat[:,0], vect, mat[:,2]), axis = 1)
     mat3 = np.stack((mat[:,0], mat[:,1], vect), axis = 1)
-    det = np.linalg.det
     d = det(mat)
     d1 = det(mat1)
     d2 = det(mat2)
     d3 = det(mat3)
-    return np.array((d1 / d, d2 / d, d3 / d))
+    return (d1 / d, d2 / d, d3 / d)
 
 def parabolicApprox(pointArr):
     xArr = pointArr[:,0]
     yArr = pointArr[:,1]
-    x4sum = np.sum(xArr ** 4)
-    x3sum = np.sum(xArr ** 3)
-    x2sum = np.sum(xArr ** 2)
-    x1sum = np.sum(xArr)
-    x0sum = xArr.shape[0]
-    yx2sum = np.sum(yArr * xArr ** 2)
-    yx1sum = np.sum(yArr * xArr)
-    yx0sum = np.sum(yArr)
-    mat = np.array([[x4sum, x3sum, x2sum],
-                    [x3sum, x2sum, x1sum],
-                    [x2sum, x1sum, x0sum]])
-    vect = np.array([yx2sum, yx1sum, yx0sum])
+    x4sum = np.sum(xArr ** 4, axis = 0)
+    x3sum = np.sum(xArr ** 3, axis = 0)
+    x2sum = np.sum(xArr ** 2, axis = 0)
+    x1sum = np.sum(xArr, axis = 0)
+    x0sum = np.resize(xArr.shape[0], xArr.shape[1:])
+    yx2sum = np.sum(yArr * xArr ** 2, axis = 0)
+    yx1sum = np.sum(yArr * xArr, axis = 0)
+    yx0sum = np.sum(yArr, axis = 0)
+    mat = np.stack([np.stack([x4sum, x3sum, x2sum], axis = 0),
+                    np.stack([x3sum, x2sum, x1sum], axis = 0),
+                    np.stack([x2sum, x1sum, x0sum], axis = 0)], axis = 0)
+    vect = np.stack([yx2sum, yx1sum, yx0sum], axis = 0)
     return cramer(mat, vect)
 
 #loacalisator
 
-def stackPoints(win, u):
-    t = win.shape[0]
+def coord(winArr, u, i, j):
+    xArr = np.resize(scal((i,j), u), winArr.shape[2:])
+    return np.stack((xArr, winArr[i, j]), axis = 0)
+
+def stackPoints(winArr, u):
+    t = winArr.shape[0]
     c = t / 2
-    inCircle = lambda i, j: norm((i - c, j - c)) <= c
-    scalu = lambda i, j: scal((i - c, j - c), u)
-    coord = lambda i, j: np.array((scalu(i, j), win[i, j]))
-    ind = intertools.product(range(t), repeat = 2)
-    PointList = np.array([coord for i, j in ind if inCircle(i, j)])
+    circle = circleCut(t, t / 2).astype(np.bool)
+    ind = itertools.product(range(t), repeat = 2)
+    pointGene = (coord(winArr, u, i, j) for i, j in ind if circle[i, j])
+    return np.stack(pointGene)
 
 def smooth(a, b, c, eps):
-    aplus = max(a, 0)
-    cplus = max(c, 0)
+    aplus = np.maximum(a, 0)
+    cplus = np.maximum(c, 0)
     return -(2 * aplus * cplus) / (abs(b) + eps)
 
-vstackPoints = np.vectorize(stackPoints)
-vparabolicApprox = np.vectorize(parabolicApprox)
-vsmooth = np.vectorize(smooth)
-
 def localise(arr, t, u, eps):
-    wArr = winVect(arr2D, t, t)
-    transTuple = range(2, arr.ndims) + (0, 1)
-    wArr = np.transpose(wArr, transTuple)
-    wArr = vstackPoints(wArr)
-    derivArr = vparabolicApprox(wArr)
-    derivArr = vsmooth(derivArr)
-    return derivArr
+    pp("    window vectorizing")
+    pptime()
+    wArr = winVect(arr, t, t)
+    pp("    projecting")
+    pptime()
+    wArr = stackPoints(wArr, u)
+    pp("    parabol approximating")
+    pptime()
+    parabTuple = parabolicApprox(wArr)
+    pp("    smoothing")
+    pptime()
+    smoothArr = smooth(*parabTuple, eps)
+    return smoothArr
 
 vlocalise = np.vectorize(localise)
 

python/analyse_image/script.py
@@ -1,8 +1,6 @@
 print("\nimporting modules")
 
 from ops import *
-from filts import *
-from itertools import chain
 
 MAXPOW = 8
 WINSIZE = 8
@@ -10,6 +8,7 @@ SIGMAS = [0.25, 0.5, 1, 2, 4, 8]
 NDIR = 8
 IMAGENAME = "pq1.png"
 FILTSIZE = 5
+EPS = 1
 
 #actual script
 
@@ -48,10 +47,11 @@ pmem()
 
 p("\nmeasuring responses")
 resp = lambda f: np.sum(multDim(mfftimage, f, [0,1]), axis = (0,1))
-#normResp = lambda f: resp(f) / np.sum(np.abs(mfftimage), axis = (0,1))
 respGene = lambda sigma: (resp(f) for f in fGaussGene(WINSIZE, NDIR, sigma))
 summedResp = lambda sigma: sum(r for r in respGene(sigma))
 summedRespList = [summedResp(sigma) for sigma in SIGMAS]
+ptime()
+pmem()
 
 #p("\nreducing and displaying responses")
 #red = lambda arr : cint(arr * 255 / np.max(arr))
@@ -64,7 +64,7 @@ combinedResp = np.concatenate(summedRespList, axis = 2)
 ptime()
 pmem()
 
-p("\n stacking with original image")
+p("\nstacking with original image")
 restoredCombinedResp = restoreShape(combinedResp, WINSIZE // 2, WINSIZE // 2)
 combinedImage = np.concatenate((labImage, restoredCombinedResp), axis = 2)
 p(combinedImage.shape)
@@ -86,9 +86,15 @@ pmem()
 #pmem()
 
 p("\nfiltering")
-geneGene = (gaussdGene(FILTSIZE, NDIR, sigma) for sigma in SIGMAS)
-filtGene = chain(*geneGene)
-filtImage = np.sum(filt(combinedImage, f) for f in filtGene)
+c = circleCut(FILTSIZE, FILTSIZE / 2)
+loc = lambda arr, u: localise(arr, WINSIZE, u, EPS)
+gaussFilt = lambda u, sigma: filt(combinedImage, gaussd(FILTSIZE, u, sigma))
+detectEdge = lambda u, sigma: loc(gaussFilt(u, sigma), u)
+it = itertools.product(dirGene(NDIR), SIGMAS)
+filtImage = np.sum(detectEdge(u, sigma) for u, sigma in it)
+psize(filtImage)
+ptime()
+pmem()
 
 p("\nreducing")
 for i in range(filtImage.shape[2]):