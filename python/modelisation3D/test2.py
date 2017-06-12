5ab2aa4679ab5efeb6ab2edc336fd8496d523946
TimDarcet <timothee.darcet@gmail.com>
2017-06-12 20:59:51 +0200

    caca

--- python/modelisation3D/test2.py
+++ python/modelisation3D/test2.py
@@ -1,9 +1,13 @@
-import time
+import numpy
 
+import wep
 
-n = 200000
-t0 = time.time()
-for i in range(n):
-	print(i)
-print((time.time() - t0) / n)
+l = numpy.load("../analyse_image/res/roller_poly.npy")
 
+l2 = [(a[0], a[1]) for a in l]
+
+l3 = [(l2[i], l2[i+1]) for i in range(len(l2) - 1)] + [(l2[-1], l2[0])]
+
+s = wep.silhouette((0, 0, 0), 0, (0, 0, 0), l3)
+s.plot()
+print(l3)
